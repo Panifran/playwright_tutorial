7a9bf77de2757f4eae24ee218927538cbd32c6a1
Javier <panifran2000@gmail.com>
2023-01-30 15:44:38 +0100

    op y cl odds de OU 3.5 FT

--- basics/game_odds.py
+++ basics/game_odds.py
@@ -155,7 +155,7 @@ def run(playwright: Playwright) -> None:
                 tooltip = page.locator(".tooltip")
                 text = tooltip.inner_text().split("\n")
                 pinn_opXHT = text[-1]
-                print(f"pinn_opXFT: {pinn_opXHT}")
+                print(f"pinn_opXHT: {pinn_opXHT}")
 
                 odd_cells[2].hover()
                 tooltip = page.locator(".tooltip")
@@ -165,9 +165,9 @@ def run(playwright: Playwright) -> None:
 
         # OPENING Y CLOSING ODDS OVER/UNDER FT
 
-        url25 = f"{url}#over-under;2"
-        print(url25)
-        page.goto(url25)
+        url_OU_FT = f"{url}#over-under;2"
+        print(url_OU_FT)
+        page.goto(url_OU_FT)
         page.reload()
         table_containers = page.locator("//div[@class='relative flex flex-col']").all()
 
@@ -288,6 +288,65 @@ def run(playwright: Playwright) -> None:
                                 print(f"pinn_clU25FT: {pinn_clU25FT}")
                                 print(f"pinn_opO25FT: {pinn_opO25FT}")
                                 print(f"pinn_opU25FT: {pinn_opU25FT}")
+
+            # OPENING Y CLOSING ODDS OVER/UNDER 3.5 FT
+            if "Over/Under +3.5" in table_container.text_content():
+                page.get_by_text("Over/Under +3.5").click()
+                odds_350_rows = table_container.locator(
+                    "xpath=div/div[@class='flex text-xs max-sm:h-[60px] h-9 border-b bg-gray-med_light border-bottom-solid']").all()
+                for elem in odds_350_rows:
+                    if len(elem.locator("xpath=div/a").all()) > 0:
+                        bookmaker = elem.locator("xpath=div/child::a[2]").text_content()
+                        if bookmaker == "bet365":
+                            odd_cells = elem.locator(
+                                "xpath=div[@class='flex flex-col items-center justify-center gap-1 border-r min-w-[60px] max-sm:min-w-[55px] border-black-borders'] |" +
+                                "div[@class='flex flex-col items-center justify-center gap-1 border-r min-w-[60px] max-sm:min-w-[55px] border-black-borders max-sm:border-l'] |" +
+                                "div[@class='flex flex-col items-center justify-center gap-1 border-r min-w-[60px] max-sm:min-w-[55px] bg-[#ffcf0d] border-black-borders'] | " +
+                                "div[@class='flex flex-col items-center justify-center gap-1 border-r min-w-[60px] max-sm:min-w-[55px] bg-[#ffcf0d] border-black-borders max-sm:border-l']").all()
+                            if len(odd_cells) >= 2:
+                                bet365_clO35FT = odd_cells[0].text_content()
+                                bet365_clU35FT = odd_cells[1].text_content()
+
+                                odd_cells[0].hover()
+                                tooltip = page.locator(".tooltip")
+                                text = tooltip.inner_text().split("\n")
+                                bet365_opO35FT = text[-1]
+
+                                odd_cells[1].hover()
+                                tooltip = page.locator(".tooltip")
+                                text = tooltip.inner_text().split("\n")
+                                bet365_opU35FT = text[-1]
+
+                                print(f"bet365_clO35FT: {bet365_clO35FT}")
+                                print(f"bet365_clU35FT: {bet365_clU35FT}")
+                                print(f"bet365_opO35FT: {bet365_opO35FT}")
+                                print(f"bet365_opU35FT: {bet365_opU35FT}")
+
+
+                        if bookmaker == "Pinnacle":
+                            odd_cells = elem.locator(
+                                "xpath=div[@class='flex flex-col items-center justify-center gap-1 border-r min-w-[60px] max-sm:min-w-[55px] border-black-borders'] |" +
+                                "div[@class='flex flex-col items-center justify-center gap-1 border-r min-w-[60px] max-sm:min-w-[55px] border-black-borders max-sm:border-l'] |" +
+                                "div[@class='flex flex-col items-center justify-center gap-1 border-r min-w-[60px] max-sm:min-w-[55px] bg-[#ffcf0d] border-black-borders'] | " +
+                                "div[@class='flex flex-col items-center justify-center gap-1 border-r min-w-[60px] max-sm:min-w-[55px] bg-[#ffcf0d] border-black-borders max-sm:border-l']").all()
+                            if len(odd_cells) >= 2:
+                                pinn_clO35FT = odd_cells[0].text_content()
+                                pinn_clU35FT = odd_cells[1].text_content()
+
+                                odd_cells[0].hover()
+                                tooltip = page.locator(".tooltip")
+                                text = tooltip.inner_text().split("\n")
+                                pinn_opO35FT = text[-1]
+
+                                odd_cells[1].hover()
+                                tooltip = page.locator(".tooltip")
+                                text = tooltip.inner_text().split("\n")
+                                pinn_opU35FT = text[-1]
+
+                                print(f"pinn_clO35FT: {pinn_clO35FT}")
+                                print(f"pinn_clU35FT: {pinn_clU35FT}")
+                                print(f"pinn_opO35FT: {pinn_opO35FT}")
+                                print(f"pinn_opU35FT: {pinn_opU35FT}")
 
     context.close()
     browser.close()
